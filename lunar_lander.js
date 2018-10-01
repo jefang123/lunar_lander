97937f014880d44eae884a1e40ec91836370c60e
Jeff <jeffreyfang123@gmail.com>
2018-10-02 04:18:32 +0900

    Fix gravity

--- lunar_lander.js
+++ lunar_lander.js
@@ -4,9 +4,13 @@ var ballRadius = 40;
 var x = canvas.width/2;
 var y = 30;
 var dx = 0;
-var dy = .50;
-var xspd = 0;
-var yspd = 0;
+var dy = 0;
+var ax = 0;
+var ay = 0;
+var ixspd = -.75;
+var xspd;
+var iyspd = .1;
+var yspd;
 var state = "MOVING";
 var time = 0;
 var fuel = 1000;
@@ -18,6 +22,47 @@ var angle = -90;
 var toRadian = Math.PI / 180;
 //img size seems to be 52x52
 
+var rightPressed = false;
+var leftPressed = false;
+var upPressed = false;
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+  switch (e.keyCode) {
+    case 39 :
+      rightPressed = true;
+      break;
+    case 37 : 
+      leftPressed = true;
+      break;
+    case 38 :
+      upPressed = true;
+      break;
+    case 40 : 
+      downPressed = true;
+      break;
+  }
+}
+
+function keyUpHandler(e) {
+  switch (e.keyCode) {
+    case 39 : 
+      rightPressed = false;
+      break;
+    case 37 : 
+      leftPressed = false;
+      break;
+    case 38 : 
+    debugger
+      upPressed = false;
+      break;
+    case 40 : 
+      downPressed = false;
+      break;
+
+  }
+}
 
 var states=  [
     "MOVING",
@@ -58,13 +103,36 @@ function drawBall() {
 function drawState() {
   ctx.font = "16px Arial";
   ctx.fillStyle = "#0095DD"
-  ctx.fillText("State: "+state, canvas.width-100, 20)
+  ctx.fillText("State: "+state, canvas.width-200, 20)
 }
 
 function drawVelocity() {
   ctx.font = "16px Arial";
   ctx.fillStyle = "#0095DD"
-  ctx.fillText("Velocity: "+vy, canvas.width-100, 100)
+  ctx.fillText("Velocity: "+vy, canvas.width-200, 100)
+}
+
+function drawxspd() {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD"
+  ctx.fillText("Xspd: "+Math.floor(xspd*10), 0, 120)
+}
+
+function drawyspd() {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD"
+  ctx.fillText("Yspd: "+Math.floor(yspd*10), 0, 140)
+}
+function drawax() {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD"
+  ctx.fillText("Acceleration X: "+ax, 0, 100)
+}
+
+function draway() {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD"
+  ctx.fillText("Acceleration Y: "+ay, 0, 80)
 }
 
 function drawangle() {
@@ -88,7 +156,7 @@ function drawFuel() {
 function drawDY() {
   ctx.font = "16px Arial";
   ctx.fillStyle = "#0095DD"
-  ctx.fillText("Y: "+yspd, canvas.width-100, 40)
+  ctx.fillText("Y: "+yspd, canvas.width-200, 40)
 }
 
 
@@ -96,13 +164,13 @@ function drawDY() {
 function drawDX() {
   ctx.font = "16px Arial";
   ctx.fillStyle = "#0095DD"
-  ctx.fillText("X: "+xspd, canvas.width-100, 60)
+  ctx.fillText("X: "+xspd, canvas.width-200, 60)
 }
 
 function drawatime() {
   ctx.font = "16px Arial";
   ctx.fillStyle = "#0095DD"
-  ctx.fillText("ATime: "+atime, canvas.width-100, 80)
+  ctx.fillText("ATime: "+atime, canvas.width-200, 80)
 }
 
 function draw() {
@@ -121,9 +189,13 @@ function draw() {
   img.src = 'shuttle2_0.png';
   }
   ctx.clearRect(0,0, canvas.width, canvas.height);
-  ctx.drawImage(img,x,y, 52, 52);
+  ctx.drawImage(img,x,y, 20, 20);
   drawTime();
   drawFuel();
+  drawax();
+  draway();
+  drawxspd();
+  drawyspd();
   // drawBall();
   drawState();
   drawatime();
@@ -143,76 +215,75 @@ function draw() {
   if (upPressed && fuel == 0) {
     upPressed = false
   }
+  let lastxspd;
+  let lastyspd;
   if (upPressed) {
-    yspd = Math.cos(angle*toRadian)
-    xspd = Math.sin(angle*toRadian)
-    fuel -= .05;
+    ay = Math.cos(angle*toRadian) * -.01
+    ax = Math.sin(angle*toRadian) * .01
+    ay *=.97
+    xspd += ax 
+    yspd += ay 
+    // yspd *= 1.01
+    fuel -= .1;
+    lastxpsd = xspd;
+    lastyspd = yspd;
+    if (x > canvas.width-20) {
+      x = 0;
+    }
+    if (x < 0) {
+      x = canvas.width-20;
+    }
+    if (y < 0) {
+      y = 0;
+    }
   } 
 
   if(!upPressed) {
-    yspd = 0;
-    xspd = 0;
+   
+    if (lastxspd) {
+    xpd = lastxspd;
+    lastxspd = 0; 
+    yspd = lastyspd;
+    lastyspd = 0;
+    }
+     xspd *= 1;
+    
+    yspd+= .002
+  
+
+  if (x > canvas.width-20) {
+    x = 0;
   }
- 
-  let initialv = dy*atime;
-  let velocity = yspd*atime;
-  vy = velocity
+  if (x < 0) {
+    x = canvas.width-20;
+  }
+  if (y < 0) {
+    y = 0;
+  }
+}
+
 
   if (state === states[0]) {
 
-    if (y >= (canvas.height -(30 + 52))) {
-      y = canvas.height - (30 + 52)
+    if (Math.floor(y) >= (canvas.height -(30 + 20))) {
+      y = canvas.height - (30 + 20)
       state = states[1];
       x = x;
       y = y;
       alert("game over")
     } else {
-      x += (dx+xspd);
-      y += (initialv-velocity);
+      if (!xspd) {
+        xspd = ixspd;
+        yspd = iyspd;
+      }
+      x += xspd;
+      y += yspd;
     }
   }
 
 }
 
-var rightPressed = false;
-var leftPressed = false;
-var upPressed = false;
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
 
-function keyDownHandler(e) {
-  if(e.keyCode == 39) {
-      rightPressed = true;
-     
-  }
-  else if(e.keyCode == 37) {
-      leftPressed = true;
-      
-  }
-  else if(e.keyCode == 38) {
-      upPressed = true;
-  }
-  else if(e.keyCode == 40) {
-    downPressed = true
-  }
-}
-
-function keyUpHandler(e) {
-  if(e.keyCode == 39) {
-      rightPressed = false;
-      
-  }
-  else if(e.keyCode == 37) {
-      leftPressed = false;
-  }
-  else if(e.keyCode == 38) {
-      upPressed = false;
-      atime = 1;
-  }
-  else if(e.keyCode == 40 ) {
-    downPressed = false;
-  }
-}
 
 var game;
 var ticker;
